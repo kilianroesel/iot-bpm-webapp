a761ed04e7fd80ec6751eaa4121e868375e93123
Kilian Rösel <kilian.roesel@gea.com>
2025-03-14 12:55:50 +0100

    Reformat

--- iot-bpm-frontend/src/routes/MainLayout.tsx
+++ iot-bpm-frontend/src/routes/MainLayout.tsx
@@ -10,17 +10,17 @@ export default function MainLayout() {
       <div className="flex grow">
         <SideBar />
         <div className="grow">
-        <Suspense
-          fallback={
-            <div className="flex h-screen items-center justify-center space-x-4 transition">
-              <div className="h-4 w-4 animate-bounce rounded-full bg-cyan-500"></div>
-              <div className="h-4 w-4 animate-bounce rounded-full bg-cyan-500 delay-500"></div>
-              <div className="h-4 w-4 animate-bounce rounded-full bg-cyan-500 delay-1000"></div>
-            </div>
-          }
-        >
-          <Outlet />
-        </Suspense>
+          <Suspense
+            fallback={
+              <div className="flex h-screen items-center justify-center space-x-4 transition">
+                <div className="h-4 w-4 animate-bounce rounded-full bg-cyan-500"></div>
+                <div className="h-4 w-4 animate-bounce rounded-full bg-cyan-500 delay-500"></div>
+                <div className="h-4 w-4 animate-bounce rounded-full bg-cyan-500 delay-1000"></div>
+              </div>
+            }
+          >
+            <Outlet />
+          </Suspense>
         </div>
       </div>
     </div>
